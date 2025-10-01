77986ff3f7025807b16e0908bd78e4b79f97555f
gosha <agogara132@gmail.com>
2025-10-01 12:33:32 +0300

    dd

--- project/botutils/handlers.go
+++ project/botutils/handlers.go
@@ -41,7 +41,7 @@ func HandleFloorCheck(redisClient *redis.Client,c telebot.Context,) string {
 		avgProfit := (price/1000 - avgPrice) / avgPrice * 100
 		
 		if c != nil {
-			fmt.Print("Ответил в чат %d", c.Chat().ID)
+			fmt.Printf("Ответил в чат %d\n", c.Chat().ID)
 		}
 		msg := fmt.Sprintf(
 			"цена минта: 1.4\nфлор на Heart Locket Reactor: %.2f\nфлор на кусочек: %.2f\nСредняя цена всех NFT: %.2f TON\n----------------\nпрофит по цене минта: %.2f%%\nпрофит по флору кусочков: %.2f%%\nсредний профит комьюнити: %.2f%%",
